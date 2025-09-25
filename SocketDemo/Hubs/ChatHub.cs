4e2203d3073cd21e53590eec3393073397a647ed
Lungelo Ngcemu <lungelofarm@gmail.com>
2025-09-25 19:46:10 +0200

    first commit

---
+++ SocketDemo/Hubs/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SocketDemo.Hubs
+{
+    public class ChatHub : Hub
+    {
+        // Join a channel
+        public async Task JoinChannel(string channelName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, channelName);
+           // await Clients.Group(channelName).SendAsync("ReceiveMessage", "System", $"{Context.ConnectionId} joined {channelName}");
+        }
+
+        // Leave a channel
+        public async Task LeaveChannel(string channelName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, channelName);
+            await Clients.Group(channelName).SendAsync("ReceiveMessage", "System", $"{Context.ConnectionId} left {channelName}");
+        }
+
+        // Send message to a specific channel
+        public async Task SendMessageToChannel(string channelName, string user, string message)
+        {
+            await Clients.Group(channelName).SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ SocketDemo/Program.cs
@@ -0,0 +1,49 @@
+using SocketDemo.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:5173") // frontend URL
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials(); // IMPORTANT for SignalR
+    });
+});
+
+// Add services to the container.
+builder.Services.AddSignalR();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors("CorsPolicy");
+
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<ChatHub>("/chathub");
+});
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
